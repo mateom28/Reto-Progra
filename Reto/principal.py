a294189799cb7982f98913316f479be935d98099
mateom28 <49160880+mateom28@users.noreply.github.com>
2021-02-24 23:47:57 -0500

    metodos del Drive sin Flask

---
+++ Reto/principal.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+import httplib2
+import os, io
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from apiclient.http import MediaFileUpload, MediaIoBaseDownload
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+import auth
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = 'credentials.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+authInst = auth.auth(SCOPES,CLIENT_SECRET_FILE,APPLICATION_NAME)
+credentials = authInst.getCredentials()
+
+http = credentials.authorize(httplib2.Http())
+drive_service = discovery.build('drive', 'v3', http=http)
+
+def moverArchivo(file_id):
+    folder_id = '1M86vcbwKG-70oh4LpuTGDSWPy9nTBvWa'
+    file = drive_service.files().get(fileId=file_id,
+                                    fields='parents').execute()
+    previous_parents = ",".join(file.get('parents'))
+    file = drive_service.files().update(fileId=file_id,
+                                        addParents=folder_id,
+                                        removeParents=previous_parents,
+                                        fields='id, parents').execute()
+
+def subirArchivo(filename,filepath,mimetype):
+    file_metadata = {'name': filename}
+    media = MediaFileUpload(filepath,
+                            mimetype=mimetype)
+    file = drive_service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+    print('File ID: %s' % file.get('id'))
+    moverArchivo(file.get('id'))
+
+def crearCarpeta(name):
+    file_metadata = {
+    'name': name,
+    'mimeType': 'application/vnd.google-apps.folder'
+    }
+    file = drive_service.files().create(body=file_metadata,
+                                        fields='id').execute()
+    print ('Folder ID: %s' % file.get('id'))
+    moverArchivo(file.get('id'))
+
+def buscarArchivo(size,query):
+    results = drive_service.files().list(
+    pageSize=size,fields="nextPageToken, files(id, name)",q=query).execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(item)
+            print('{0} ({1})'.format(item['name'], item['id']))
+
+
+crearCarpeta('1233')
